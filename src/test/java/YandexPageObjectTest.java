bd8934d3d8e4fa1697ed9388bd6e233bc978da8b
LexPalych <lexshcherbinin@gmail.com>
2018-12-13 14:46:52 +0300

    Initial commit

---
+++ src/test/java/YandexPageObjectTest.java
@@ -0,0 +1,159 @@
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class YandexPageObjectTest {
+    private ChromeDriver driver;
+
+    @FindBy(xpath = "//*[@data-id='market']")
+    private WebElement market;
+
+    @FindBy(xpath = "//*[@id=\"header-search\"]")
+    private WebElement search;
+
+    @FindBy(xpath = "//*[@class=\"search2__button\"]")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//*[@name='Цена от']")
+    private WebElement minPrice;
+
+    @FindBy(xpath = "//*[@name='Цена до']")
+    private WebElement maxPrice;
+
+    @FindBy(xpath = "//span[.=\"Apple\"]")
+    private WebElement companyApple;
+
+    @FindBy(xpath = "//span[.=\"ASUS\"]")
+    private WebElement companyASUS;
+
+    @FindBy(xpath = "//span[.=\"HP\"]")
+    private WebElement companyHP;
+
+    @FindBy(xpath = "//span[.=\"Xiaomi\"]")
+    private WebElement companyXiaomi;
+
+    @FindBy(xpath = "//span[.=\"Core i7\"]")
+    private WebElement intelCore;
+
+    @FindBy(xpath = "//strong[contains(.,'Толстовка')]")
+    private WebElement sweatshirt;
+
+    public YandexPageObjectTest(ChromeDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public YandexPageObjectTest openSite(String url, ChromeDriver chromeDriver) {
+        chromeDriver.get(url);
+        return this;
+    }
+
+    public YandexPageObjectTest tabMarket() {
+        market.click();
+        return this;
+    }
+
+    public YandexPageObjectTest fillLaptop() {
+        search.sendKeys("Ноутбуки");
+        return this;
+    }
+
+    public YandexPageObjectTest checkFieldLaptop() {
+        assertEquals(search.getAttribute("value"), "Ноутбуки");
+        return this;
+    }
+
+    public YandexPageObjectTest tabSearch() {
+        searchButton.click();
+        return this;
+    }
+
+    public YandexPageObjectTest fillMinPrice() {
+        minPrice.sendKeys("100000");
+        return this;
+    }
+
+    public YandexPageObjectTest fillMaxPrice() {
+        maxPrice.sendKeys("200000");
+        return this;
+    }
+
+    public YandexPageObjectTest checkMinPrice() {
+        assertEquals(minPrice.getAttribute("value"), "100000");
+        return this;
+    }
+
+    public YandexPageObjectTest checkMaxPrice() {
+        assertEquals(maxPrice.getAttribute("value"), "200000");
+        return this;
+    }
+
+    public YandexPageObjectTest tabIntelCore() {
+        intelCore.click();
+        return this;
+    }
+
+    public YandexPageObjectTest tabCompanyApple() {
+        companyApple.click();
+        return this;
+    }
+
+    public YandexPageObjectTest tabCompanyASUS() {
+        companyASUS.click();
+        return this;
+    }
+
+    public YandexPageObjectTest tabCompanyHP() {
+        companyHP.click();
+        return this;
+    }
+
+    public YandexPageObjectTest tabCompanyXiaomi() {
+        companyXiaomi.click();
+        return this;
+    }
+
+    public YandexPageObjectTest checkEnabledCompanyApple() {
+        assertTrue(companyApple.isEnabled());
+        return this;
+    }
+
+    public YandexPageObjectTest checkEnabledCompanyASUS() {
+        assertTrue(companyASUS.isEnabled());
+        return this;
+    }
+
+    public YandexPageObjectTest checkEnabledCompanyHP() {
+        assertTrue(companyHP.isEnabled());
+        return this;
+    }
+
+    public YandexPageObjectTest checkEnabledCompanyXiaomi() {
+        assertTrue(companyXiaomi.isEnabled());
+        return this;
+    }
+
+    public YandexPageObjectTest claerSearch() {
+        search.clear();
+        return this;
+    }
+
+    public YandexPageObjectTest fillGreenElephant() {
+        search.sendKeys("Зеленый слоник");
+        return this;
+    }
+
+    public YandexPageObjectTest tabSweatshirt() {
+        try {
+            sweatshirt.click();
+        }catch (NoSuchElementException thrown){
+            System.out.println("Упс, что-то пошло не так. 'Толстовка' не найдена");
+        }
+        return this;
+    }
+}
---
+++ src/test/java/YandexTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class YandexTest {
+
+    private static ChromeDriver driver;
+    private static String url = "https://yandex.ru/";
+
+    @BeforeAll
+    public static void beforeTest() {
+        System.out.println("Запуск теста");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Alexandr\\IdeaProjects\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        //Чтобы окошко развернулось на весь экран
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void YandexTest() {
+        YandexPageObjectTest test = new YandexPageObjectTest(driver);
+        // Заходим на Яндекс => Яндекс.Маркет => Вводим "Ноутбуки" => Проверяем => Нажимаем "Найти"
+        test
+            .openSite(url, driver)
+            .tabMarket()
+            .fillLaptop()
+            .checkFieldLaptop()
+            .tabSearch()
+
+        // Вводим минимум и максимум цены и проверяем их
+            .fillMinPrice()
+            .fillMaxPrice()
+            .checkMinPrice()
+            .checkMaxPrice()
+
+        //Ставим галочки напротив нужных фирм
+            .tabIntelCore()
+            .tabCompanyApple()
+            .tabCompanyASUS()
+            .tabCompanyHP()
+            .tabCompanyXiaomi()
+
+        // Проверяем, что нужные фирмы отмечены
+            .checkEnabledCompanyApple()
+            .checkEnabledCompanyASUS()
+            .checkEnabledCompanyHP()
+            .checkEnabledCompanyXiaomi()
+
+        // Очищаем поле поиска => Вводим "Зелёный слоник" => Нажимаем "Найти" => Ищем "Толстовка"
+            .claerSearch()
+            .fillGreenElephant()
+            .tabSearch()
+            .tabSweatshirt()
+            .openSite(url, driver);
+    }
+
+    @AfterAll
+    public static void afterTest() {
+        System.out.println("Тест окончен");
+        driver.quit();
+    }
+
+}
