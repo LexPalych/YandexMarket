1d95df7d814f55fe74487fa15e2657ffa48cc87b
LexPalych <lexshcherbinin@gmail.com>
2018-12-27 17:43:29 +0300

    Добавил логи

--- src/main/java/pageObject/Page.java
+++ src/main/java/pageObject/Page.java
@@ -1,5 +1,7 @@
 package pageObject;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,7 +11,11 @@ import java.util.List;
 public class Page<T extends Page> {
     protected WebDriver driver;
 
+    protected static final Logger logger = LogManager.getLogger(Page.class);
+
     public <D> D tabClick(String name, Class<D> expectedPage) {
+        logger.warn(("Hello, World!"));
+        logger.error(("Hello, World!"));
         getWebElement(getString(name)).click();
         return createPage(expectedPage);
     }
